11154fb258bcffd014179e6d965e403fbe8376ed
Sandun <SandunWebDev@gmail.com>
2022-01-10 13:32:21 +0530

    feat/initial-setup: Update Husky and Lint Staged configs

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-	'*.+(js|jsx|css|less|scss|graphql|gql|html|json|md|markdown|mdown|mkdn|mdx|ts|tsx|vue|yaml|yml)':
+	// Linting Staged Files
+	'*.+(js|jsx|ts|tsx|css|less|scss|graphql|gql|html|json|md|markdown|mdown|mkdn|mdx|ts|tsx|vue|yaml|yml)':
 		['prettier --write'],
-	'*.+(js|jsx)': ["eslint --fix'"],
+	'*.+(js|jsx|ts|tsx)': ["eslint --fix'"],
 	'*.+(css|scss|sass|less)': ['stylelint --fix'],
 };
