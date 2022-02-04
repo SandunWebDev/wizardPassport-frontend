8740e9f8a42d5a61d88a32d243aa1c7a063ee952
Sandun <SandunWebDev@gmail.com>
2022-02-04 14:32:05 +0530

    feat/scrollBar: Add custom scrollbar to app

--- src/pages/root/App/App.tsx
+++ src/pages/root/App/App.tsx
@@ -4,6 +4,7 @@ import { Box, Progress } from '@chakra-ui/react';
 import { Outlet, useRouter } from 'react-location';
 import { ReactLocationDevtools } from 'react-location-devtools';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import SimpleBar from 'simplebar-react';
 
 import WebStatusIndicators from '../../../components/widgets/WebStatusIndicators/WebStatusIndicators';
 import './App.css';
@@ -13,32 +14,40 @@ function App() {
 
 	return (
 		<div className='App'>
-			{/* Just showing some navigation indicator when route with asynchronous loading is happening.
+			{/* Custom Scroll Bar. Style overrides are done on "styles/overrides/simplebarReact.css" */}
+			<SimpleBar
+				style={{ height: '100vh', width: '100vw' }}
+				autoHide
+				timeout={1000}
+				scrollbarMinSize={50}
+				scrollbarMaxSize={500}>
+				{/* Just showing some navigation indicator when route with asynchronous loading is happening.
 			    But, If it take longer than "defaultPendingMs/pendingMs" value, Then what defined in "defaultPendingElement/pendingElement" will be shown.  */}
-			{router.pending ? (
-				<Box top='0' position='absolute' width='100%'>
-					<Progress size='sm' bg='none' isIndeterminate />
-				</Box>
-			) : null}
+				{router.pending ? (
+					<Box top='0' position='absolute' width='100%'>
+						<Progress size='sm' bg='none' isIndeterminate />
+					</Box>
+				) : null}
 
-			{/*	Below component will render "Matching route path's element, If applicable"	*/}
-			<Outlet />
+				{/*	Below component will render "Matching route path's element, If applicable"	*/}
+				<Outlet />
 
-			{/*	Below component will render "ReactLocation Dev. Tools" & "ReactQuery Dev Tools", Only in Development Mode */}
-			<ReactLocationDevtools initialIsOpen={false} position='bottom-left' />
-			<ReactQueryDevtools
-				initialIsOpen={false}
-				position='bottom-left'
-				toggleButtonProps={{
-					style: {
-						position: 'fixed',
-						left: '50px',
-					},
-				}}
-			/>
+				{/*	Below component will render "ReactLocation Dev. Tools" & "ReactQuery Dev Tools", Only in Development Mode */}
+				<ReactLocationDevtools initialIsOpen={false} position='bottom-left' />
+				<ReactQueryDevtools
+					initialIsOpen={false}
+					position='bottom-left'
+					toggleButtonProps={{
+						style: {
+							position: 'fixed',
+							left: '50px',
+						},
+					}}
+				/>
 
-			{/* Some Global Level Indicators */}
-			<WebStatusIndicators />
+				{/* Some Global Level Indicators */}
+				<WebStatusIndicators />
+			</SimpleBar>
 		</div>
 	);
 }
